90c40b2a4732b86330a162d445623a5d04c24d77
hieu1966hn <hieu1966hn@gmail.com>
2022-02-12 18:41:35 +0700

    done city nam + Weather state

--- SS8/js/index.js
+++ SS8/js/index.js
@@ -1,3 +1,5 @@
+const APP_ID = '651daf0af47eb49a9b65a0007afcce53'
+
 const searchInput = document.getElementById("search-input");
 
 const cityName = document.querySelector(".city-name");
@@ -10,5 +12,29 @@ const sunrise = document.querySelector(".sunrise");
 const sunset = document.querySelector(".sunset");
 const humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind-speed");
+
+
+searchInput.addEventListener("change", (event) => {
+  // fetch(): Phương thức mặc định của Javascript để Gọi API từ Server của web khác về
+  // web mình.
+
+  console.log(event.target.value);
+  // In ra dữ liệu mình nhập vào.
+
+  /// Kéo dữ liệu từ WEB Openweathermap
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${APP_ID}&lang=vi&units=metric `)
+    .then(async function (response) {
+      const data = await response.json();
+
+      console.log(data);
+      cityName.innerHTML = data.name;
+      weatherState.innerHTML = data.weather[0].description
+
+
+
+    })
+  // .then(data => console.log(data));
+
+})
 
 
